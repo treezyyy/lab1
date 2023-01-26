b027bf52137695b68075e54edde2481c9493d286
treezyyy <122749977+treezyyy@users.noreply.github.com>
2023-01-26 21:25:11 +0300

    added third test

--- test.py
+++ test.py
@@ -7,3 +7,4 @@ def find_average(numbers):
 
 print(find_average([1, 2, 3]), 'it is first test')
 print(find_average([30, 50, 70, 90]), 'it is second test')
+print(find_average([100, 100, 100]), 'it is third test')
